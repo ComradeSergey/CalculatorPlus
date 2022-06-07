2cf7d21ba3886f932a876f4e1d81027a1b6ce56e
Эктоморф <exe898@ya.ru>
2022-06-07 08:56:26 +0400

    added program restart via loop while

--- src/CalculatorPlus.java
+++ src/CalculatorPlus.java
@@ -4,39 +4,40 @@ import java.util.Scanner;
 public class CalculatorPlus {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Программа калькулятор+ ПРИВЕТСТВУЕТ ВАС!");
-        System.out.println("Предлагаем Вам произвести математические операции над числами. Какую операцию вы хотите " +
-                "произвести?");
-        System.out.println("Для умножения введите * с клавиатуры, для деления кнопку / . ");
-        System.out.println("Для сложение введите + с клавиатуры, для вычитания кнопку - . ");
+        while (true) {
+            System.out.println("Программа калькулятор+ ПРИВЕТСТВУЕТ ВАС!");
+            System.out.println("Предлагаем Вам произвести математические операции над числами. Какую операцию вы хотите " +
+                    "произвести?");
+            System.out.println("Для умножения введите * с клавиатуры, для деления кнопку / . ");
+            System.out.println("Для сложение введите + с клавиатуры, для вычитания кнопку - . ");
 
-        int code = System.in.read();
-        char q = (char) code;
+            int code = System.in.read();
+            char q = (char) code;
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Для начала через кнопку Enter введите два значения, для получения результата");
-        int a = in.nextInt();
-        int b = in.nextInt();
+            Scanner in = new Scanner(System.in);
+            System.out.println("Для начала через кнопку Enter введите два значения, для получения результата");
+            int a = in.nextInt();
+            int b = in.nextInt();
 
-        if (q == '+') {
-            int с = sum(a, b);
-            print("Результат сложения = " + с);
-        } else if (q == '-') {
-            int c = diff(a, b);
-            print("Результат вычитания = " + c);
-        } else if (q == '*') {
-            int c = times(a, b);
-            print("Результат умножения = " + c);
-        } else if (q == '/') {
-            if (check(b)) {
-                System.out.println("Делить на ноль нельзя");
-            } else {
-                int c = per(a, b);
-                print("Результат деления = " + c);
+            if (q == '+') {
+                int с = sum(a, b);
+                System.out.println("Результат сложения = " + с);
+            } else if (q == '-') {
+                int c = diff(a, b);
+                System.out.println("Результат вычитания = " + c);
+            } else if (q == '*') {
+                int c = times(a, b);
+                System.out.println("Результат умножения = " + c);
+            } else if (q == '/') {
+                if (check(b)) {
+                    System.out.println("Делить на ноль нельзя");
+                } else {
+                    int c = per(a, b);
+                    System.out.println("Результат деления = " + c);
+                }
             }
         }
     }
-
     private static int sum(int a, int b) {
         return a + b;
     }
